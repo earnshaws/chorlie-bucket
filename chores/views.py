6da4912efafd78f1d9adc42a4b955a27d05d5bc0
Berton Earnshaw <berton.earnshaw@gmail.com>
2017-02-07 23:36:00 -0700

    Finish authentication

--- chores/views.py
+++ chores/views.py
@@ -1,6 +1,17 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import View
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the chores app")
+class LoggedInMixin(object):
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(LoggedInMixin, self).dispatch(*args, **kwargs)
+
+
+class IndexView(LoggedInMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'chores/chores.html')
--- chorlie_bucket/settings.py
+++ chorlie_bucket/settings.py
@@ -126,4 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/chores'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/chores/'
